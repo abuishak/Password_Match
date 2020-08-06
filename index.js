20bd0c55edaf0d1398121a87d61160b6e4bc7fb0
abuishak <39011865+abuishak@users.noreply.github.com>
2020-08-06 14:49:34 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+  $("h1").text("Password Match")
+  $("#ioginButton").click(function(){
+var password1 = $("#pass1").val();
+var password2 = $("#pass2").val();
+
+if (password1!="" && password2!=""){
+
+   if(password1 == password2){
+      alert("successfully login")
+      
+   }else{
+      alert("password mismatch")
+   }
+}else{
+   alert("please enter password")
+}
+
+   })
